b8a730f22b4f637935cab549b809aedfacd24f89
Mark Cox <markcox80@gmail.com>
2013-06-05 07:54:11 +1000

    Completed the documentation for PREPARE-GROUP-CHAINS-SORT-TEST.

--- src/querying.lisp
+++ src/querying.lisp
@@ -97,3 +97,46 @@ EXPRESSION can be one of:
 		 (funcall fn
 			  (slot-value a-task slot-name)
 			  (slot-value b-task slot-name)))))))))))
+
+(defun prepare-group-chains-sort-test (expression)
+  "Return an arity two predicate that can be used to sort a collection
+  of chains. The predicate returned is governed by the value of
+  EXPRESSION.
+  
+  The descriptions below use CHAIN-A to refer to the first argument of
+  the returned predicate and CHAIN-B for the second argument of this
+  predicate. Additionally, the values TASK-A and TASK-B represent the
+  tasks found in CHAINS-A and CHAINS-B respectively.
+
+ (> symbol name)
+
+   The symbol denotes a name of a TASK class, and NAME is the name of
+   a slot of that class. See (> TASK-CLASS TASK-DIRECT-SLOT-DEFINITION)
+   for the value that is returned.
+
+ (> TASK-CLASS TASK-DIRECT-SLOT-DEFINITION)
+
+   The function returned returns non-NIL if both chains contain a task
+   which is of type TASK-CLASS and the value of the slot represented
+   by TASK-DIRECT-SLOT-DEFINITION in TASK-A is /greater than/ the
+   corresponding value in TASK-B. The comparison function used is
+   obtained by invoking the TEST>-FUNCTION generic function on
+   TASK-DIRECT-SLOT-DEFINITION.
+
+ (< symbol name)
+ (< TASK-CLASS TASK-DIRECT-SLOT-DEFINITION)
+
+   The predicate returned is identical to (> TASK-CLASS
+   TASK-DIRECT-SLOT-DEFINITION) except that the slot comparison
+   function used is obtained by invoking the TEST<-FUNCTION generic
+   function on TASK-DIRECT-SLOT-DEFINITION.
+
+ (:classes &rest classes)
+ (:classes &rest TASK-CLASSes)
+
+   The predicate returned returns non-NIL if CHAINS-A contains a task
+   whose class has a lower position in the sequence TASK-CLASSes when
+   compared with the task obtained from CHAIN-B. An error is signalled
+   if no task inheriting from one of the TASK-CLASSes can be found in
+   either CHAINS-A or CHAINS-B.
+")
